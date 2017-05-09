b13958c4c18c594b44d1041ebee6396ee9418ce5
ophcha <ophelie.charbit@gmail.com>
2017-05-09 11:51:16 +0200

    début d'authentification (et beaucoup d'imagination...)

---
+++ src/fr/viacesi/cpaimport/Main.java
@@ -0,0 +1,10 @@
+package fr.viacesi.cpaimport;
+
+public class Main {
+	public static void main(String[] args){ 
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur.getNom();
+		utilisateur.getPrenom();
+		utilisateur.getRole();
+	}
+}
---
+++ src/fr/viacesi/cpaimport/Utilisateur.java
@@ -0,0 +1,19 @@
+package fr.viacesi.cpaimport;
+
+public class Utilisateur {
+	String nom;
+	String prenom;
+	int role;
+	
+	public void getNom(){
+	    System.out.println(nom);
+	  } 
+	
+	public void getPrenom(){
+		System.out.println(prenom);
+	}
+	
+	public void getRole(){
+		System.out.println(role);
+	}
+}
